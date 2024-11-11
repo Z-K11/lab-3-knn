4e5c0a001a760ca9deeb7db952e70809fbd287ed
Z-K11 <zulqarnainbazidkhail@gmail.com>
2024-11-11 21:33:35 +0500

    Read data from csv

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 import pandas as pd
 import numpy as np
+#start from here
+
+readable  = pd.read_csv('teleCust1000t.csv')
+print(readable.head())
